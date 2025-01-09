29d2a2cc8e284e7221f395450c223f1b56a5d290
Garba Jamiu <garbajamiu2000@gmail.com>
2025-01-09 20:51:12 +0100

    feat: implement the form schema for signup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,39 @@
+import AuthHeader from "@/features/Header/AuthHeader";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+//TOdo:
+// Form schema - zod
+// Get the value from form
+// intergate supabe auth
+
+// Form schema , what do i need ?
+// email, username ,password, confirm password
+
+const FormSchema = z.object({
+  email: z.string().min(1, { message: "Email is required" }),
+  username: z.string().min(1, { message: "Username is required" }),
+  password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
+  confirm_password: z
+    .string()
+    .min(8, { message: "Confirm password is required" })
+    .refine((data) => data.password === data.confirm_password, { message: "Password don't match", path: ["confirm_password"] }),
+});
+
 function Signup() {
-  return <div> Sign up page</div>;
+  const form = useForm({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  return (
+    <div>
+      <AuthHeader text="Sign up" />p
+    </div>
+  );
 }
 
 export default Signup;
--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -12,7 +12,7 @@ import {
   // NavigationMenuViewport,
 } from "@/components/ui/navigation-menu";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
 interface componentsType {
   title: string;
@@ -63,35 +63,42 @@ const components: componentsType[] = [
 
 function Header() {
   return (
-    <nav className="flex justify-between">
+    <nav className="flex items-center justify-between">
       <p>Airdrop Finder</p>
 
-      <NavigationMenu>
-        <NavigationMenuList className="flex gap-6">
-          {components.map((comp) => (
-            <div key={comp.url}>
-              {comp?.children ? (
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>{comp.title}</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    {comp.children.map((child) => (
-                      <Link key={child.title} href={child.url} legacyBehavior passHref>
-                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>{child.title}</NavigationMenuLink>
-                      </Link>
-                    ))}
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-              ) : (
-                <NavigationMenuItem key={comp.title}>
-                  <Link href={comp.url} legacyBehavior passHref>
-                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>{comp.title}</NavigationMenuLink>
-                  </Link>
-                </NavigationMenuItem>
-              )}
-            </div>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div className="flex gap-6  items-center">
+        <NavigationMenu>
+          <NavigationMenuList className="flex gap-6">
+            {components.map((comp) => (
+              <div key={comp.url}>
+                {comp?.children ? (
+                  <NavigationMenuItem>
+                    <NavigationMenuTrigger>{comp.title}</NavigationMenuTrigger>
+                    <NavigationMenuContent>
+                      {comp.children.map((child) => (
+                        <Link key={child.title} href={child.url} legacyBehavior passHref>
+                          <NavigationMenuLink className={navigationMenuTriggerStyle()}>{child.title}</NavigationMenuLink>
+                        </Link>
+                      ))}
+                    </NavigationMenuContent>
+                  </NavigationMenuItem>
+                ) : (
+                  <NavigationMenuItem key={comp.title}>
+                    <Link href={comp.url} legacyBehavior passHref>
+                      <NavigationMenuLink className={navigationMenuTriggerStyle()}>{comp.title}</NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                )}
+              </div>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+
+        <div className="flex gap-3">
+          <Link href="/signin">Sign in</Link>
+          <Link href="/signup">Sign up</Link>
+        </div>
+      </div>
     </nav>
   );
 }
